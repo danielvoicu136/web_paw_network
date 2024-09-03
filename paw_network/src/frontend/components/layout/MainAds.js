2f84d92018c5e47518e0d64c529bd9874b4d1f3c
Daniel Voicu <danielvoicu136@gmail.com>
2024-09-03 16:48:00 +0300

    navbar creation

--- paw_network/src/frontend/components/layout/MainAds.js
+++ paw_network/src/frontend/components/layout/MainAds.js
@@ -4,7 +4,7 @@ import classes from './MainAds.module.css';
 function MainAds() { 
     return (
         <div className={classes['ads-container']}>
-            <div className={classes['small-box-1']}><p><span className={classes.accentText}>Unlimited</span> services for your pets</p></div>
+            <div className={classes['small-box-1']}><p><span className={classes.accentText}>Unlimited</span> services for your <span className={classes.accentText}>all pets</span></p></div>
             <div className={classes['small-box-2']}><p><span className={classes.accentText}>-60%</span> | 3 months premium <span className={classes.accentText}>CODE: PET60</span></p></div>
             <div className={classes['big-box']}></div>
         </div>
--- paw_network/src/frontend/components/layout/MainNavigation.js
+++ paw_network/src/frontend/components/layout/MainNavigation.js
@@ -1,22 +1,38 @@
 import { Link } from "react-router-dom";
+import classes from './MainNavigation.module.css';
 
 function MainNavigation() { 
 
     return (
-      
-        <header>
-        <div className="main-logo">Logo</div>
-        <nav>
-                <ul>  
-                    <li><Link to='/'>Index</Link></li>
-                    <li><Link to='/home'>Home</Link></li>
-                    <li><Link to='/services'>Services</Link></li>
-                </ul>    
-        </nav>
+        <header className={classes.header}>
+            <nav className={classes.navbar}>
+                <div className={classes.navbarLeft}>
+                    <a href="#" className={classes.logo}><i className="fa-solid fa-paw"></i> PAW</a>
+
+                    <div className={classes.dropdown}>
+                        <a href="#" className={classes.dropbtn}><i className="fas fa-home"></i> Explore Routing</a>
+                        <div className={classes.dropdownContent}>
+                            <Link to='/'>Index</Link>
+                            <Link to='/home'>Home</Link>
+                            <Link to='/services'>Services</Link>
+                        </div>
+                    </div>
+
+                    <div className={classes.dropdown}>
+                        <a href="#" className={classes.dropbtn}><i class="fa-solid fa-gift"></i> Paw Gift</a>
+                        <div className={classes.dropdownContent}>
+                            <a href="#">Buy Gift</a>
+                            <a href="#">Activate Gift ?</a>
+                        </div>
+                    </div>
+                </div>
+                <div className={classes.navbarRight}>
+                    <a href="#"><i class="fa-solid fa-user"></i> My Account</a>
+                    <a href="#" className={classes.ctaButton}><i class="fa-solid fa-right-to-bracket"></i> Enter in our Paw World</a>
+                </div>
+            </nav>
         </header>
-
     );
-
 }
 
 export default MainNavigation;
