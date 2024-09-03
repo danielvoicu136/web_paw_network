e6bbe3eca79e44173bfc6baeec6881fdd775f638
Daniel Voicu <danielvoicu136@gmail.com>
2024-09-03 17:27:53 +0300

    nav bar

--- paw_network/src/frontend/components/layout/MainNavigation.js
+++ paw_network/src/frontend/components/layout/MainNavigation.js
@@ -7,10 +7,10 @@ function MainNavigation() {
         <header className={classes.header}>
             <nav className={classes.navbar}>
                 <div className={classes.navbarLeft}>
-                    <a href="#" className={classes.logo}><i className="fa-solid fa-paw"></i> PAW</a>
+                    <a href="#" className={classes.logo}><span className={classes.logoIcon}><i className="fa-solid fa-paw"></i></span> PAW</a>
 
                     <div className={classes.dropdown}>
-                        <a href="#" className={classes.dropbtn}><i className="fas fa-home"></i> Explore Routing</a>
+                        <a href="#" className={classes.dropbtn}><i className="fas fa-home"></i> Explore</a>
                         <div className={classes.dropdownContent}>
                             <Link to='/'>Index</Link>
                             <Link to='/home'>Home</Link>
@@ -19,7 +19,7 @@ function MainNavigation() {
                     </div>
 
                     <div className={classes.dropdown}>
-                        <a href="#" className={classes.dropbtn}><i class="fa-solid fa-gift"></i> Paw Gift</a>
+                        <a href="#" className={classes.dropbtn}><i class="fa-solid fa-gift"></i> Gifts</a>
                         <div className={classes.dropdownContent}>
                             <a href="#">Buy Gift</a>
                             <a href="#">Activate Gift ?</a>
@@ -27,8 +27,9 @@ function MainNavigation() {
                     </div>
                 </div>
                 <div className={classes.navbarRight}>
+                    <a href="#" className={classes.searchIcon}><i class="fa-solid fa-magnifying-glass"></i></a>
                     <a href="#"><i class="fa-solid fa-user"></i> My Account</a>
-                    <a href="#" className={classes.ctaButton}><i class="fa-solid fa-right-to-bracket"></i> Enter in our Paw World</a>
+                    <a href="#" className={classes.ctaButton}><i class="fa-solid fa-right-to-bracket"></i> Register</a>
                 </div>
             </nav>
         </header>
