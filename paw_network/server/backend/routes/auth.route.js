399cc23485b438d90f00586a3fd7b833a411fcf4
Daniel Voicu <danielvoicu136@gmail.com>
2024-09-19 14:02:39 +0300

    structure update

--- paw_network/server/backend/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-import { login, logout, signup } from "../controllers/auth.controller.js";
-import express from express
-
-const router = express.Router();
-
-router.get("/signup", signup)
-router.get("/login", login)
-router.get("/logout", logout)
-
