3caf870948528746d5e3f99ca6d88ed00a25123b
Daniel Voicu <danielvoicu136@gmail.com>
2024-09-23 10:40:40 +0300

    backendroot

--- server/server.js
+++ server/server.js
@@ -12,6 +12,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Hello, World! Your MERN app is deployed.');
+  });
+
 app.use("/api/auth", authRoutes);
 
 
